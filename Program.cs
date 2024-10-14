4ebdb5e59498061eae2a9a536cb9a62e5589dfa6
Nicholas-Chum-Student <nichae3@student.oaisd.org>
2024-10-14 13:34:54 +0000

    Created a calculator that takes a number and performs an operation.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+    Console.WriteLine("Provide a number to perform an operation.");
+    double num1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Provide a second number to perform an operation.");
+    double num2 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Provide an operation to perform the operation.");
+    string oper = Console.ReadLine();
+
+    if (oper.Contains("+"))
+    {
+        double num3 = num1 + num2;
+        Console.WriteLine(num2 + ", alright! That's simple. " + num1 + " + " + num2 + " is " + num3 + ".");
+    }
+    else if (oper.Contains("-"))
+    {
+        double num3 = num1 - num2;
+        Console.WriteLine(num2 + ", alright! That's simple. " + num1 + " - " + num2 + " is " + num3 + ".");
+    }
+    else if (oper.Contains("/"))
+    {
+        double num3 = num1 / num2;
+        Console.WriteLine(num2 + ", alright! That's simple. " + num1 + " / " + num2 + " is " + num3 + ".");
+    } 
+    else if (oper.Contains("*"))
+    {
+        double num3 = num1 * num2;
+        Console.WriteLine(num2 + ", alright! That's simple. " + num1 + " * " + num2 + " is " + num3 + ".");
+    }
+    else if (oper.Contains("%"))
+    {
+        double num3 = num1 % num2;
+        Console.WriteLine(num2 + ", alright! That's simple. " + num1 + " % " + num2 + " is " + num3 + ".");
+    }
+    }
+}
